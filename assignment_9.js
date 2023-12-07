952e422a08c0ff87fdbcb21ec660283ba6ac490a
JerahRich <147950409+JerahRich@users.noreply.github.com>
2023-12-07 00:03:59 -0500

    Add files via upload

--- assignment_9.js
+++ assignment_9.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
-    // Initializations
+  
     const deleteCompletedBtn = $('#delete-completed-btn');
     const deleteAllBtn = $('#delete-all-btn');
-    const newTaskNameInput = $('#newTaskName'); // Added reference to the input field
-    const addTaskBtn = $('#addTaskBtn');
+    const newTaskNameInput = $('#newTaskName'); 
 
-    // Event listeners
+   
     deleteCompletedBtn.on('click', deleteCompletedTasks);
     deleteAllBtn.on('click', deleteAllTasks);
 
-    // Function to create a task item
     function createTaskItem(taskText, isCompleted = false) {
         const listItem = $('<li>').addClass('list-group-item task-item d-flex justify-content-between align-items-center');
         const taskDiv = $('<div>').addClass('task-text').text(taskText).toggleClass('completed', isCompleted);
@@ -18,40 +16,34 @@ $(document).ready(function () {
         return listItem.append(taskDiv, checkbox);
     }
 
-    // Function to add a new task
     function addTask() {
         const taskText = newTaskNameInput.val();
         if (taskText.trim() !== '') {
             const listItem = createTaskItem(taskText);
             $('#task-list').append(listItem);
             $('#addTaskModal').modal('hide');
-            newTaskNameInput.val(''); // Clear the input field after adding the task
+            newTaskNameInput.val('');
         }
     }
 
-    // Function to toggle task completion
     function toggleTaskCompleted() {
         const listItem = $(this).parent();
         listItem.toggleClass('completed');
     }
 
-    // Function to delete completed tasks
     function deleteCompletedTasks() {
         $('.task-item.completed').remove();
     }
 
-    // Function to delete all tasks
     function deleteAllTasks() {
         $('#task-list').empty();
     }
 
-    // Event listener for the Add Task button in the modal
     addTaskBtn.on('click', addTask);
 
-    // Event listener for pressing Enter in the input field
     newTaskNameInput.on('keypress', function (e) {
         if (e.which === 13) {
-            // 13 is the key code for Enter
+           
             addTask();
         }
     });
